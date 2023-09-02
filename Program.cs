9fa39ab4307dd21b12f7f359193608c646331340
Aysegul ESKI <ayseguleskk@gmail.com>
2023-09-02 20:22:16 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+
+        int[] dizi={4,9,6,72,15,43,21};
+        
+        foreach (var sayi in dizi)
+        {
+            Console.WriteLine(sayi);
+        }
+
+        Console.WriteLine("SORT");
+        Array.Sort(dizi);
+        foreach (var sayi in dizi)
+        {
+            Console.WriteLine(sayi);
+        }
+
+        Console.WriteLine("CLEAR");
+        Array.Clear(dizi,5,2);
+        foreach (var sayi in dizi)
+        {
+            Console.WriteLine(sayi);
+        }
+
+        Console.WriteLine("REVERSE");
+        Array.Reverse(dizi);
+        foreach (var sayi in dizi)
+        {
+            Console.WriteLine(sayi);
+        }
+
+        Console.WriteLine("INDEXOF");
+        Console.WriteLine(Array.IndexOf(dizi,21));
+
+        Console.WriteLine("RESİZE");
+        Array.Resize<int>(ref dizi,12);
+        dizi[10]=54;
+        foreach (var sayi in dizi)
+        {
+            Console.WriteLine(sayi);
+        }
+    }
+}
